ba4d08cb435e6ca0a5fb3bd4980439ed9f684931
shucheng <shucheng@bd-apaas.com>
2022-10-11 22:12:42 +0800

    v2.8.1

--- .beagle/registry.beagle.default.sh
+++ .beagle/registry.beagle.default.sh
@@ -3,17 +3,37 @@ set -e
 
 mkdir -p ./.vscode
 
-if [ -e ./.vscode/registry.beagle.default.key ]; then
-  echo "registry.beagle.default.key is ready!"
-else
-  /usr/bin/openssl genrsa -out ./.vscode/registry.beagle.default.key 2048
-  echo "registry.beagle.default.key is ready!"
+if ! [ -e ./.vscode/registry.beagle.default.key ]; then
+  /usr/bin/openssl genrsa \
+                  -out ./.vscode/registry.beagle.default.key 2048
+
+  /usr/bin/openssl req -new \
+                  -key ./.vscode/registry.beagle.default.key \
+                  -out ./.vscode/registry.beagle.default.csr \
+                  -subj "/CN=registry/C=CN/ST=BeiJing/L=Beijing/O=beaglecloud/OU=System" \
+                  -config ./.beagle/registry.beagle.default.cnf
+                  
+  /usr/bin/openssl x509 -req \
+                  -in ./.vscode/registry.beagle.default.csr \
+                  -CA ./.vscode/ca.crt \
+                  -CAkey ./.vscode/ca.key \
+                  -CAcreateserial -out ./.vscode/registry.beagle.default.crt \
+                  -days 3650 -extensions v3_req \
+                  -extfile ./.beagle/registry.beagle.default.cnf
 fi
 
-if [ -e ./.vscode/registry.beagle.default.crt ]; then
-  echo "registry.beagle.default.crt is ready!"
-else
-  /usr/bin/openssl req -new -key ./.vscode/registry.beagle.default.key -out ./.vscode/registry.beagle.default.csr -subj "/CN=registry/C=CN/ST=BeiJing/L=Beijing/O=beaglecloud/OU=System" -config ./.beagle/registry.beagle.default.cnf
-  /usr/bin/openssl x509 -req -in ./.vscode/registry.beagle.default.csr -CA ./.vscode/ca.crt -CAkey ./.vscode/ca.key -CAcreateserial -out ./.vscode/registry.beagle.default.crt -days 3650 -extensions v3_req -extfile ./.beagle/registry.beagle.default.cnf
-  echo "registry.beagle.default.crt is ready!"
+if ! [ -e ./.vscode/registry.beagle.default.crt ]; then
+  /usr/bin/openssl req -new \
+                  -key ./.vscode/registry.beagle.default.key \
+                  -out ./.vscode/registry.beagle.default.csr \
+                  -subj "/CN=registry/C=CN/ST=BeiJing/L=Beijing/O=beaglecloud/OU=System" \
+                  -config ./.beagle/registry.beagle.default.cnf
+                  
+  /usr/bin/openssl x509 -req \
+                  -in ./.vscode/registry.beagle.default.csr \
+                  -CA ./.vscode/ca.crt \
+                  -CAkey ./.vscode/ca.key \
+                  -CAcreateserial -out ./.vscode/registry.beagle.default.crt \
+                  -days 3650 -extensions v3_req \
+                  -extfile ./.beagle/registry.beagle.default.cnf
 fi
